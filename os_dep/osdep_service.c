67ccf5eeeee9ac445c46a37656f6c35cd5e98e00
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2014-08-20 12:33:49 +0200

    remove some FREEBSD code
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- os_dep/osdep_service.c
+++ os_dep/osdep_service.c
@@ -83,9 +83,6 @@ inline u8* _rtw_vmalloc(u32 sz)
 #ifdef PLATFORM_LINUX
 	pbuf = vmalloc(sz);
 #endif
-#ifdef PLATFORM_FREEBSD
-	pbuf = malloc(sz,M_DEVBUF,M_NOWAIT);
-#endif
 
 #ifdef PLATFORM_WINDOWS
 	NdisAllocateMemoryWithTag(&pbuf,sz, RT_TAG);
@@ -111,9 +108,6 @@ inline u8* _rtw_zvmalloc(u32 sz)
 	if (pbuf != NULL)
 		memset(pbuf, 0, sz);
 #endif
-#ifdef PLATFORM_FREEBSD
-	pbuf = malloc(sz,M_DEVBUF,M_ZERO|M_NOWAIT);
-#endif
 #ifdef PLATFORM_WINDOWS
 	NdisAllocateMemoryWithTag(&pbuf,sz, RT_TAG);
 	if (pbuf != NULL)
@@ -127,9 +121,6 @@ inline void _rtw_vmfree(u8 *pbuf, u32 sz)
 {
 #ifdef	PLATFORM_LINUX
 	vfree(pbuf);
-#endif
-#ifdef PLATFORM_FREEBSD
-	free(pbuf,M_DEVBUF);
 #endif
 #ifdef PLATFORM_WINDOWS
 	NdisFreeMemory(pbuf,sz, 0);
@@ -157,9 +148,6 @@ u8* _rtw_malloc(u32 sz)
 		pbuf = kmalloc(sz,in_interrupt() ? GFP_ATOMIC : GFP_KERNEL);
 
 #endif
-#ifdef PLATFORM_FREEBSD
-	pbuf = malloc(sz,M_DEVBUF,M_NOWAIT);
-#endif
 #ifdef PLATFORM_WINDOWS
 
 	NdisAllocateMemoryWithTag(&pbuf,sz, RT_TAG);
@@ -182,9 +170,6 @@ u8* _rtw_malloc(u32 sz)
 
 u8* _rtw_zmalloc(u32 sz)
 {
-#ifdef PLATFORM_FREEBSD
-	return malloc(sz,M_DEVBUF,M_ZERO|M_NOWAIT);
-#else // PLATFORM_FREEBSD
 	u8 	*pbuf = _rtw_malloc(sz);
 
 	if (pbuf != NULL) {
@@ -200,7 +185,6 @@ u8* _rtw_zmalloc(u32 sz)
 	}
 
 	return pbuf;
-#endif // PLATFORM_FREEBSD
 }
 
 void	_rtw_mfree(u8 *pbuf, u32 sz)
@@ -214,9 +198,6 @@ void	_rtw_mfree(u8 *pbuf, u32 sz)
 #endif
 		kfree(pbuf);
 
-#endif
-#ifdef PLATFORM_FREEBSD
-	free(pbuf,M_DEVBUF);
 #endif
 #ifdef PLATFORM_WINDOWS
 
@@ -488,7 +469,7 @@ void rtw_mfree2d(void *pbuf, int h, int w, int size)
 int	_rtw_memcmp(void *dst, void *src, u32 sz)
 {
 
-#if defined (PLATFORM_LINUX)|| defined (PLATFORM_FREEBSD)
+#if defined (PLATFORM_LINUX)
 //under Linux/GNU/GLibc, the return value of memcmp for two same mem. chunk is 0
 
 	if (!(memcmp(dst, src, sz)))
@@ -507,66 +488,10 @@ int	_rtw_memcmp(void *dst, void *src, u32 sz)
 		return _FALSE;
 
 #endif
-
-
-
 }
 
-#ifdef PLATFORM_FREEBSD
 
-static inline void __list_add(_list *pnew, _list *pprev, _list *pnext)
- {
-         pnext->prev = pnew;
-         pnew->next = pnext;
-         pnew->prev = pprev;
-         pprev->next = pnew;
-}
 
-//review again
-struct sk_buff * dev_alloc_skb(unsigned int size)
-{
-	struct sk_buff *skb=NULL;
-    	u8 *data=NULL;
-
-	//skb = (struct sk_buff *)_rtw_zmalloc(sizeof(struct sk_buff)); // for skb->len, etc.
-	skb = (struct sk_buff *)_rtw_malloc(sizeof(struct sk_buff));
-	if(!skb)
-		goto out;
-	data = _rtw_malloc(size);
-	if(!data)
-		goto nodata;
-
-	skb->head = (unsigned char*)data;
-	skb->data = (unsigned char*)data;
-	skb->tail = (unsigned char*)data;
-	skb->end = (unsigned char*)data + size;
-	skb->len = 0;
-	//printf("%s()-%d: skb=%p, skb->head = %p\n", __FUNCTION__, __LINE__, skb, skb->head);
-
-out:
-	return skb;
-nodata:
-	_rtw_mfree((u8 *)skb, sizeof(struct sk_buff));
-	skb = NULL;
-goto out;
-
-}
-
-void dev_kfree_skb_any(struct sk_buff *skb)
-{
-	//printf("%s()-%d: skb->head = %p\n", __FUNCTION__, __LINE__, skb->head);
-	if(skb->head)
-		_rtw_mfree(skb->head, 0);
-	//printf("%s()-%d: skb = %p\n", __FUNCTION__, __LINE__, skb);
-	if(skb)
-		_rtw_mfree((u8 *)skb, 0);
-}
-struct sk_buff *skb_clone(const struct sk_buff *skb)
-{
-	return NULL;
-}
-
-#endif
 
 
 void _rtw_init_listhead(_list *list)
@@ -578,10 +503,6 @@ void _rtw_init_listhead(_list *list)
 
 #endif
 
-#ifdef PLATFORM_FREEBSD
-         list->next = list;
-         list->prev = list;
-#endif
 #ifdef PLATFORM_WINDOWS
 
         NdisInitializeListHead(list);
@@ -607,15 +528,6 @@ u32	rtw_is_list_empty(_list *phead)
 		return _FALSE;
 
 #endif
-#ifdef PLATFORM_FREEBSD
-
-	if (phead->next == phead)
-		return _TRUE;
-	else
-		return _FALSE;
-
-#endif
-
 
 #ifdef PLATFORM_WINDOWS
 
@@ -636,10 +548,6 @@ void rtw_list_insert_head(_list *plist, _list *phead)
 	list_add(plist, phead);
 #endif
 
-#ifdef PLATFORM_FREEBSD
-	__list_add(plist, phead, phead->next);
-#endif
-
 #ifdef PLATFORM_WINDOWS
 	InsertHeadList(phead, plist);
 #endif
@@ -651,11 +559,6 @@ void rtw_list_insert_tail(_list *plist, _list *phead)
 #ifdef PLATFORM_LINUX
 
 	list_add_tail(plist, phead);
-
-#endif
-#ifdef PLATFORM_FREEBSD
-
-	__list_add(plist, phead->prev, phead);
 
 #endif
 #ifdef PLATFORM_WINDOWS
@@ -672,9 +575,6 @@ void rtw_init_timer(_timer *ptimer, void *padapter, void *pfunc)
 
 #ifdef PLATFORM_LINUX
 	_init_timer(ptimer, adapter->pnetdev, pfunc, adapter);
-#endif
-#ifdef PLATFORM_FREEBSD
-	_init_timer(ptimer, adapter->pifp, pfunc, adapter->mlmepriv.nic_hdl);
 #endif
 #ifdef PLATFORM_WINDOWS
 	_init_timer(ptimer, adapter->hndis_adapter, pfunc, adapter->mlmepriv.nic_hdl);
@@ -696,9 +596,6 @@ void _rtw_init_sema(_sema	*sema, int init_val)
 	sema_init(sema, init_val);
 
 #endif
-#ifdef PLATFORM_FREEBSD
-	sema_init(sema, init_val, "rtw_drv");
-#endif
 #ifdef PLATFORM_OS_XP
 
 	KeInitializeSemaphore(sema, init_val,  SEMA_UPBND); // count=0;
@@ -714,9 +611,6 @@ void _rtw_init_sema(_sema	*sema, int init_val)
 
 void _rtw_free_sema(_sema	*sema)
 {
-#ifdef PLATFORM_FREEBSD
-	sema_destroy(sema);
-#endif
 #ifdef PLATFORM_OS_CE
 	CloseHandle(*sema);
 #endif
@@ -730,9 +624,6 @@ void _rtw_up_sema(_sema	*sema)
 
 	up(sema);
 
-#endif
-#ifdef PLATFORM_FREEBSD
-	sema_post(sema);
 #endif
 #ifdef PLATFORM_OS_XP
 
@@ -755,10 +646,6 @@ u32 _rtw_down_sema(_sema *sema)
 	else
 		return _SUCCESS;
 
-#endif
-#ifdef PLATFORM_FREEBSD
-	sema_wait(sema);
-	return  _SUCCESS;
 #endif
 #ifdef PLATFORM_OS_XP
 
@@ -789,9 +676,6 @@ void	_rtw_mutex_init(_mutex *pmutex)
 #endif
 
 #endif
-#ifdef PLATFORM_FREEBSD
-	mtx_init(pmutex, "", NULL, MTX_DEF|MTX_RECURSE);
-#endif
 #ifdef PLATFORM_OS_XP
 
 	KeInitializeMutex(pmutex, 0);
@@ -813,9 +697,6 @@ void	_rtw_mutex_free(_mutex *pmutex)
 #else
 #endif
 
-#ifdef PLATFORM_FREEBSD
-	sema_destroy(pmutex);
-#endif
 
 #endif
 
@@ -836,9 +717,6 @@ void	_rtw_spinlock_init(_lock *plock)
 	spin_lock_init(plock);
 
 #endif
-#ifdef PLATFORM_FREEBSD
-		mtx_init(plock, "", NULL, MTX_DEF|MTX_RECURSE);
-#endif
 #ifdef PLATFORM_WINDOWS
 
 	NdisAllocateSpinLock(plock);
@@ -849,9 +727,6 @@ void	_rtw_spinlock_init(_lock *plock)
 
 void	_rtw_spinlock_free(_lock *plock)
 {
-#ifdef PLATFORM_FREEBSD
-	 mtx_destroy(plock);
-#endif
 
 #ifdef PLATFORM_WINDOWS
 
@@ -860,28 +735,6 @@ void	_rtw_spinlock_free(_lock *plock)
 #endif
 
 }
-#ifdef PLATFORM_FREEBSD
-extern PADAPTER prtw_lock;
-
-void rtw_mtx_lock(_lock *plock){
-	if(prtw_lock){
-		mtx_lock(&prtw_lock->glock);
-	}
-	else{
-		printf("%s prtw_lock==NULL",__FUNCTION__);
-	}
-}
-void rtw_mtx_unlock(_lock *plock){
-	if(prtw_lock){
-		mtx_unlock(&prtw_lock->glock);
-	}
-	else{
-		printf("%s prtw_lock==NULL",__FUNCTION__);
-	}
-
-}
-#endif //PLATFORM_FREEBSD
-
 
 void	_rtw_spinlock(_lock	*plock)
 {
@@ -890,9 +743,6 @@ void	_rtw_spinlock(_lock	*plock)
 
 	spin_lock(plock);
 
-#endif
-#ifdef PLATFORM_FREEBSD
-	mtx_lock(plock);
 #endif
 #ifdef PLATFORM_WINDOWS
 
@@ -910,9 +760,6 @@ void	_rtw_spinunlock(_lock *plock)
 	spin_unlock(plock);
 
 #endif
-#ifdef PLATFORM_FREEBSD
-	mtx_unlock(plock);
-#endif
 #ifdef PLATFORM_WINDOWS
 
 	NdisReleaseSpinLock(plock);
@@ -929,9 +776,6 @@ void	_rtw_spinlock_ex(_lock	*plock)
 	spin_lock(plock);
 
 #endif
-#ifdef PLATFORM_FREEBSD
-	mtx_lock(plock);
-#endif
 #ifdef PLATFORM_WINDOWS
 
 	NdisDprAcquireSpinLock(plock);
@@ -947,9 +791,6 @@ void	_rtw_spinunlock_ex(_lock *plock)
 
 	spin_unlock(plock);
 
-#endif
-#ifdef PLATFORM_FREEBSD
-	mtx_unlock(plock);
 #endif
 #ifdef PLATFORM_WINDOWS
 
@@ -990,11 +831,6 @@ u32	rtw_get_current_time(void)
 #ifdef PLATFORM_LINUX
 	return jiffies;
 #endif
-#ifdef PLATFORM_FREEBSD
-	struct timeval tvp;
-	getmicrotime(&tvp);
-	return tvp.tv_sec;
-#endif
 #ifdef PLATFORM_WINDOWS
 	LARGE_INTEGER	SystemTime;
 	NdisGetCurrentSystemTime(&SystemTime);
@@ -1007,9 +843,6 @@ inline u32 rtw_systime_to_ms(u32 systime)
 #ifdef PLATFORM_LINUX
 	return systime * 1000 / HZ;
 #endif
-#ifdef PLATFORM_FREEBSD
-	return systime * 1000;
-#endif
 #ifdef PLATFORM_WINDOWS
 	return systime / 10000 ;
 #endif
@@ -1019,9 +852,6 @@ inline u32 rtw_ms_to_systime(u32 ms)
 {
 #ifdef PLATFORM_LINUX
 	return ms * HZ / 1000;
-#endif
-#ifdef PLATFORM_FREEBSD
-	return ms /1000;
 #endif
 #ifdef PLATFORM_WINDOWS
 	return ms * 10000 ;
@@ -1034,9 +864,6 @@ inline s32 rtw_get_passing_time_ms(u32 start)
 #ifdef PLATFORM_LINUX
 	return rtw_systime_to_ms(jiffies-start);
 #endif
-#ifdef PLATFORM_FREEBSD
-	return rtw_systime_to_ms(rtw_get_current_time());
-#endif
 #ifdef PLATFORM_WINDOWS
 	LARGE_INTEGER	SystemTime;
 	NdisGetCurrentSystemTime(&SystemTime);
@@ -1048,9 +875,6 @@ inline s32 rtw_get_time_interval_ms(u32 start, u32 end)
 {
 #ifdef PLATFORM_LINUX
 	return rtw_systime_to_ms(end-start);
-#endif
-#ifdef PLATFORM_FREEBSD
-	return rtw_systime_to_ms(rtw_get_current_time());
 #endif
 #ifdef PLATFORM_WINDOWS
 	return rtw_systime_to_ms(end-start);
@@ -1076,10 +900,6 @@ void rtw_sleep_schedulable(int ms)
     return;
 
 #endif
-#ifdef PLATFORM_FREEBSD
-	DELAY(ms*1000);
-	return ;
-#endif
 
 #ifdef PLATFORM_WINDOWS
 
@@ -1097,11 +917,6 @@ void rtw_msleep_os(int ms)
 
   	msleep((unsigned int)ms);
 
-#endif
-#ifdef PLATFORM_FREEBSD
-       //Delay for delay microseconds
-	DELAY(ms*1000);
-	return ;
 #endif
 #ifdef PLATFORM_WINDOWS
 
@@ -1122,12 +937,6 @@ void rtw_usleep_os(int us)
       else
 		msleep( (us/1000) + 1);
 
-#endif
-#ifdef PLATFORM_FREEBSD
-	//Delay for delay microseconds
-	DELAY(us);
-
-	return ;
 #endif
 #ifdef PLATFORM_WINDOWS
 
@@ -1199,10 +1008,6 @@ void rtw_mdelay_os(int ms)
    	mdelay((unsigned long)ms);
 
 #endif
-#ifdef PLATFORM_FREEBSD
-	DELAY(ms*1000);
-	return ;
-#endif
 #ifdef PLATFORM_WINDOWS
 
 	NdisStallExecution(ms*1000); //(us)*1000=(ms)
@@ -1219,11 +1024,6 @@ void rtw_udelay_os(int us)
       udelay((unsigned long)us);
 
 #endif
-#ifdef PLATFORM_FREEBSD
-	//Delay for delay microseconds
-	DELAY(us);
-	return ;
-#endif
 #ifdef PLATFORM_WINDOWS
 
 	NdisStallExecution(us); //(us)
@@ -1236,9 +1036,6 @@ void rtw_udelay_os(int us)
 void rtw_yield_os()
 {
 #ifdef PLATFORM_LINUX
-	yield();
-#endif
-#ifdef PLATFORM_FREEBSD
 	yield();
 #endif
 #ifdef PLATFORM_WINDOWS
@@ -1676,75 +1473,6 @@ error:
 }
 #endif
 
-#ifdef PLATFORM_FREEBSD
-/*
- * Copy a buffer from userspace and write into kernel address
- * space.
- *
- * This emulation just calls the FreeBSD copyin function (to
- * copy data from user space buffer into a kernel space buffer)
- * and is designed to be used with the above io_write_wrapper.
- *
- * This function should return the number of bytes not copied.
- * I.e. success results in a zero value.
- * Negative error values are not returned.
- */
-unsigned long
-copy_from_user(void *to, const void *from, unsigned long n)
-{
-        if ( copyin(from, to, n) != 0 ) {
-                /* Any errors will be treated as a failure
-                   to copy any of the requested bytes */
-                return n;
-        }
-
-        return 0;
-}
-
-unsigned long
-copy_to_user(void *to, const void *from, unsigned long n)
-{
-	if ( copyout(from, to, n) != 0 ) {
-		/* Any errors will be treated as a failure
-		   to copy any of the requested bytes */
-		return n;
-	}
-
-	return 0;
-}
-
-
-/*
- * The usb_register and usb_deregister functions are used to register
- * usb drivers with the usb subsystem. In this compatibility layer
- * emulation a list of drivers (struct usb_driver) is maintained
- * and is used for probing/attaching etc.
- *
- * usb_register and usb_deregister simply call these functions.
- */
-int
-usb_register(struct usb_driver *driver)
-{
-        rtw_usb_linux_register(driver);
-        return 0;
-}
-
-
-int
-usb_deregister(struct usb_driver *driver)
-{
-        rtw_usb_linux_deregister(driver);
-        return 0;
-}
-
-void module_init_exit_wrapper(void *arg)
-{
-        int (*func)(void) = arg;
-        func();
-        return;
-}
-
-#endif //PLATFORM_FREEBSD
 
 #ifdef CONFIG_PLATFORM_SPRD
 #ifdef do_div
@@ -1759,8 +1487,6 @@ u64 rtw_modular64(u64 x, u64 y)
 	return do_div(x, y);
 #elif defined(PLATFORM_WINDOWS)
 	return (x % y);
-#elif defined(PLATFORM_FREEBSD)
-	return (x %y);
 #endif
 }
 
@@ -1770,8 +1496,6 @@ u64 rtw_division64(u64 x, u64 y)
 	do_div(x, y);
 	return x;
 #elif defined(PLATFORM_WINDOWS)
-	return (x / y);
-#elif defined(PLATFORM_FREEBSD)
 	return (x / y);
 #endif
 }

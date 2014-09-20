0a70fb5967ba56c71f503ee213e24178d3ee26bc
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2014-09-20 14:08:14 +0200

    checkpatch fixes
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- hal/rtl8812a/usb/rtl8812au_led.c
+++ hal/rtl8812a/usb/rtl8812au_led.c
@@ -19,326 +19,262 @@
  ******************************************************************************/
 #define _RTL8812AU_LED_C_
 
-//#include <drv_types.h>
 #include <rtl8812a_hal.h>
 
-//================================================================================
-// LED object.
-//================================================================================
-
-
-//================================================================================
-//	Prototype of protected function.
-//================================================================================
-
-
-//================================================================================
-// LED_819xUsb routines.
-//================================================================================
-
-//
-//	Description:
-//		Turn on LED according to LedPin specified.
-//
-static void
-SwLedOn_8812AU(
-	PADAPTER		padapter,
-	PLED_USB		pLed
-)
+static void SwLedOn_8812AU(PADAPTER padapter,PLED_USB pLed)
 {
 	uint8_t	LedCfg;
 	HAL_DATA_TYPE	*pHalData = GET_HAL_DATA(padapter);
 
-	if( (padapter->bSurpriseRemoved == _TRUE) || ( padapter->bDriverStopped == _TRUE))
-	{
+	if ((padapter->bSurpriseRemoved == _TRUE) || ( padapter->bDriverStopped == _TRUE)) {
 		return;
 	}
 
-	if(	RT_GetInterfaceSelection(padapter) == INTF_SEL2_MINICARD ||
-	 	RT_GetInterfaceSelection(padapter) == INTF_SEL3_USB_Solo ||
-		RT_GetInterfaceSelection(padapter) == INTF_SEL4_USB_Combo)
-	{
+	if (RT_GetInterfaceSelection(padapter) == INTF_SEL2_MINICARD
+	 || RT_GetInterfaceSelection(padapter) == INTF_SEL3_USB_Solo
+	 || RT_GetInterfaceSelection(padapter) == INTF_SEL4_USB_Combo) {
 		LedCfg = rtw_read8(padapter, REG_LEDCFG2);
 		RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("In SwLedON,LedAddr:%X LEDPIN=%d\n",REG_LEDCFG2, pLed->LedPin));
-		switch(pLed->LedPin)
-		{
-			case LED_PIN_GPIO0:
-				break;
+		switch (pLed->LedPin) {
+		case LED_PIN_GPIO0:
+			break;
 
-			case LED_PIN_LED0:
-				RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("In SwLedOn,LedAddr:%X LEDPIN=%d\n",REG_LEDCFG2, pLed->LedPin));
-				LedCfg = rtw_read8(padapter, REG_LEDCFG2);
-				rtw_write8(padapter, REG_LEDCFG2, (LedCfg&0xf0)|BIT5|BIT6); // SW control led0 on.
-				break;
+		case LED_PIN_LED0:
+			RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("In SwLedOn,LedAddr:%X LEDPIN=%d\n",REG_LEDCFG2, pLed->LedPin));
+			LedCfg = rtw_read8(padapter, REG_LEDCFG2);
+			rtw_write8(padapter, REG_LEDCFG2, (LedCfg&0xf0)|BIT5|BIT6); // SW control led0 on.
+			break;
 
-			case LED_PIN_LED1:
-				rtw_write8(padapter, REG_LEDCFG2, (LedCfg&0x0f)|BIT5); // SW control led1 on.
-				break;
+		case LED_PIN_LED1:
+			rtw_write8(padapter, REG_LEDCFG2, (LedCfg&0x0f)|BIT5); // SW control led1 on.
+			break;
 
-			default:
-				break;
+		default:
+			break;
 		}
-	}
-	else
-	{
-		switch(pLed->LedPin)
-		{
-			case LED_PIN_GPIO0:
-				break;
+	} else {
+		switch (pLed->LedPin) {
+		case LED_PIN_GPIO0:
+			break;
 
-			case LED_PIN_LED0:
-				if(pHalData->AntDivCfg==0)
-				{
-					LedCfg = rtw_read8(padapter, REG_LEDCFG0);
-					rtw_write8(padapter, REG_LEDCFG0, (LedCfg&0x70)|BIT5); // SW control led0 on.
-					RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("SwLedOn LED0 0x%x\n", rtw_read32(padapter, REG_LEDCFG0)));
-				}
-				else
-				{
-					LedCfg = rtw_read8(padapter, REG_LEDCFG2);
-					rtw_write8(padapter, REG_LEDCFG2, (LedCfg&0xe0)|BIT7|BIT6|BIT5); // SW control led0 on.
-					RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("SwLedOn LED0 Addr 0x%x,  0x%x\n", REG_LEDCFG2, rtw_read32(padapter, REG_LEDCFG2)));
-				}
-				break;
+		case LED_PIN_LED0:
+			if (pHalData->AntDivCfg==0) {
+				LedCfg = rtw_read8(padapter, REG_LEDCFG0);
+				rtw_write8(padapter, REG_LEDCFG0, (LedCfg&0x70)|BIT5); // SW control led0 on.
+				RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("SwLedOn LED0 0x%x\n", rtw_read32(padapter, REG_LEDCFG0)));
+			} else {
+				LedCfg = rtw_read8(padapter, REG_LEDCFG2);
+				rtw_write8(padapter, REG_LEDCFG2, (LedCfg&0xe0)|BIT7|BIT6|BIT5); // SW control led0 on.
+				RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("SwLedOn LED0 Addr 0x%x,  0x%x\n", REG_LEDCFG2, rtw_read32(padapter, REG_LEDCFG2)));
+			}
+			break;
 
-			case LED_PIN_LED1:
-				LedCfg = rtw_read8(padapter, (REG_LEDCFG1));
-				rtw_write8(padapter, (REG_LEDCFG1), (LedCfg&0x70)|BIT5); // SW control led1 on.
-				RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("SwLedOn LED1 0x%x\n", rtw_read32(padapter, REG_LEDCFG1)));
-				break;
+		case LED_PIN_LED1:
+			LedCfg = rtw_read8(padapter, (REG_LEDCFG1));
+			rtw_write8(padapter, (REG_LEDCFG1), (LedCfg&0x70)|BIT5); // SW control led1 on.
+			RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("SwLedOn LED1 0x%x\n", rtw_read32(padapter, REG_LEDCFG1)));
+			break;
 
-			case LED_PIN_LED2:
-				LedCfg = rtw_read8(padapter, (REG_LEDCFG2));
-				rtw_write8(padapter, (REG_LEDCFG2), (LedCfg&0x70)|BIT5); // SW control led1 on.
-				RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("SwLedOn LED2 0x%x\n", rtw_read32(padapter, REG_LEDCFG2)));
-				break;
+		case LED_PIN_LED2:
+			LedCfg = rtw_read8(padapter, (REG_LEDCFG2));
+			rtw_write8(padapter, (REG_LEDCFG2), (LedCfg&0x70)|BIT5); // SW control led1 on.
+			RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("SwLedOn LED2 0x%x\n", rtw_read32(padapter, REG_LEDCFG2)));
+			break;
 
-			default:
-				break;
+		default:
+			break;
 		}
 	}
 
 	pLed->bLedOn = _TRUE;
 }
 
+/*
+ * 	Description:
+ * 	Turn off LED according to LedPin specified.
+ */
 
-//
-//	Description:
-//		Turn off LED according to LedPin specified.
-//
-static void
-SwLedOff_8812AU(
-	PADAPTER		padapter,
-	PLED_USB		pLed
-)
+static void SwLedOff_8812AU(PADAPTER padapter, PLED_USB	pLed)
 {
 	uint8_t	LedCfg;
 	HAL_DATA_TYPE	*pHalData = GET_HAL_DATA(padapter);
 
-	if(padapter->bSurpriseRemoved == _TRUE)
-	{
+	if (padapter->bSurpriseRemoved == _TRUE) {
 		return;
 	}
 
-	if(	RT_GetInterfaceSelection(padapter) == INTF_SEL2_MINICARD ||
-		RT_GetInterfaceSelection(padapter) == INTF_SEL3_USB_Solo ||
-		RT_GetInterfaceSelection(padapter) == INTF_SEL4_USB_Combo)
-	{
+	if (RT_GetInterfaceSelection(padapter) == INTF_SEL2_MINICARD
+	 || RT_GetInterfaceSelection(padapter) == INTF_SEL3_USB_Solo
+	 || RT_GetInterfaceSelection(padapter) == INTF_SEL4_USB_Combo) {
 		RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("In SwLedOff,LedAddr:%X LEDPIN=%d\n",REG_LEDCFG2, pLed->LedPin));
 		LedCfg = rtw_read8(padapter, REG_LEDCFG2);
 
-		// 2009/10/23 MH Issau eed to move the LED GPIO from bit  0 to bit3.
-		// 2009/10/26 MH Issau if tyhe device is 8c DID is 0x8176, we need to enable bit6 to
-		// enable GPIO8 for controlling LED.
-		// 2010/07/02 Supprt Open-drain arrangement for controlling the LED. Added by Roger.
-		//
-		switch(pLed->LedPin)
-		{
+		/*
+		 * 2009/10/23 MH Issau eed to move the LED GPIO from bit  0 to bit3.
+		 * 2009/10/26 MH Issau if tyhe device is 8c DID is 0x8176, we need to enable bit6 to
+		 * enable GPIO8 for controlling LED.
+		 * 2010/07/02 Supprt Open-drain arrangement for controlling the LED. Added by Roger.
+		 */
+		switch (pLed->LedPin) {
+		case LED_PIN_GPIO0:
+			break;
 
-			case LED_PIN_GPIO0:
-				break;
-
-			case LED_PIN_LED0:
-				if(pHalData->bLedOpenDrain == _TRUE)
-				{
-					LedCfg &= 0x90; // Set to software control.
-					rtw_write8(padapter, REG_LEDCFG2, (LedCfg|BIT3));
-					LedCfg = rtw_read8(padapter, REG_MAC_PINMUX_CFG);
-					LedCfg &= 0xFE;
-					rtw_write8(padapter, REG_MAC_PINMUX_CFG, LedCfg);
-				}
-				else
-				{
-					rtw_write8(padapter, REG_LEDCFG2, (LedCfg|BIT3|BIT5|BIT6));
-				}
-				break;
-
-			case LED_PIN_LED1:
-				LedCfg &= 0x0f; // Set to software control.
+		case LED_PIN_LED0:
+			if (pHalData->bLedOpenDrain == _TRUE) {
+				LedCfg &= 0x90; 	/* Set to software control. */
 				rtw_write8(padapter, REG_LEDCFG2, (LedCfg|BIT3));
-				break;
+				LedCfg = rtw_read8(padapter, REG_MAC_PINMUX_CFG);
+				LedCfg &= 0xFE;
+				rtw_write8(padapter, REG_MAC_PINMUX_CFG, LedCfg);
+			} else {
+				rtw_write8(padapter, REG_LEDCFG2, (LedCfg|BIT3|BIT5|BIT6));
+			}
+			break;
 
-			default:
-				break;
+		case LED_PIN_LED1:
+			LedCfg &= 0x0f; 	/* Set to software control. */
+			rtw_write8(padapter, REG_LEDCFG2, (LedCfg|BIT3));
+			break;
+
+		default:
+			break;
 		}
-	}
-	else
-	{
-		switch(pLed->LedPin)
-		{
-			case LED_PIN_GPIO0:
-				break;
+	} else {
+		switch (pLed->LedPin) {
+		case LED_PIN_GPIO0:
+			break;
 
-			case LED_PIN_LED0:
-				 if(pHalData->AntDivCfg==0)
-				{
-					LedCfg = rtw_read8(padapter, REG_LEDCFG0);
-					LedCfg &= 0x70; // Set to software control.
-					rtw_write8(padapter, REG_LEDCFG0, (LedCfg|BIT3|BIT5));
-					RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("SwLedOff LED0 0x%x\n", rtw_read32(padapter, REG_LEDCFG0)));
-				}
-				else
-				{
-					LedCfg = rtw_read8(padapter, REG_LEDCFG2);
-					LedCfg &= 0xe0; // Set to software control.
-					rtw_write8(padapter, REG_LEDCFG2, (LedCfg|BIT3|BIT7|BIT6|BIT5));
-					RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("SwLedOff LED0 0x%x\n", rtw_read32(padapter, REG_LEDCFG2)));
-				}
-				break;
-
-			case LED_PIN_LED1:
-				LedCfg = rtw_read8(padapter, REG_LEDCFG1);
-				LedCfg &= 0x70; // Set to software control.
-				rtw_write8(padapter, REG_LEDCFG1, (LedCfg|BIT3|BIT5));
-				RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("SwLedOff LED1 0x%x\n", rtw_read32(padapter, REG_LEDCFG1)));
-				break;
-
-			case LED_PIN_LED2:
+		case LED_PIN_LED0:
+			 if(pHalData->AntDivCfg==0) {
+				LedCfg = rtw_read8(padapter, REG_LEDCFG0);
+				LedCfg &= 0x70; 	/* Set to software control. */
+				rtw_write8(padapter, REG_LEDCFG0, (LedCfg|BIT3|BIT5));
+				RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("SwLedOff LED0 0x%x\n", rtw_read32(padapter, REG_LEDCFG0)));
+			} else {
 				LedCfg = rtw_read8(padapter, REG_LEDCFG2);
-				LedCfg &= 0x70; // Set to software control.
-				rtw_write8(padapter, REG_LEDCFG2, (LedCfg|BIT3|BIT5));
-				RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("SwLedOff LED1 0x%x\n", rtw_read32(padapter, REG_LEDCFG2)));
-				break;
+				LedCfg &= 0xe0; 	/* Set to software control. */
+				rtw_write8(padapter, REG_LEDCFG2, (LedCfg|BIT3|BIT7|BIT6|BIT5));
+				RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("SwLedOff LED0 0x%x\n", rtw_read32(padapter, REG_LEDCFG2)));
+			}
+			break;
 
-			default:
-				break;
+		case LED_PIN_LED1:
+			LedCfg = rtw_read8(padapter, REG_LEDCFG1);
+			LedCfg &= 0x70; 	/* Set to software control. */
+			rtw_write8(padapter, REG_LEDCFG1, (LedCfg|BIT3|BIT5));
+			RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("SwLedOff LED1 0x%x\n", rtw_read32(padapter, REG_LEDCFG1)));
+			break;
+
+		case LED_PIN_LED2:
+			LedCfg = rtw_read8(padapter, REG_LEDCFG2);
+			LedCfg &= 0x70; 	/* Set to software control. */
+			rtw_write8(padapter, REG_LEDCFG2, (LedCfg|BIT3|BIT5));
+			RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("SwLedOff LED1 0x%x\n", rtw_read32(padapter, REG_LEDCFG2)));
+			break;
+
+		default:
+			break;
 		}
 	}
 
 	pLed->bLedOn = _FALSE;
 }
 
-//
-//	Description:
-//		Turn on LED according to LedPin specified.
-//
-static void
-SwLedOn_8821AU(
-	PADAPTER		Adapter,
-	PLED_USB		pLed
-)
+/*
+ * Description:
+ * Turn on LED according to LedPin specified.
+ */
+
+static void SwLedOn_8821AU(PADAPTER Adapter, PLED_USB pLed)
 {
 	uint8_t	LedCfg;
 
-	if( (Adapter->bSurpriseRemoved == _TRUE) || ( Adapter->bDriverStopped == _TRUE))
-	{
+	if ((Adapter->bSurpriseRemoved == _TRUE) || (Adapter->bDriverStopped == _TRUE)) {
 		return;
 	}
 
-	if(	RT_GetInterfaceSelection(Adapter) == INTF_SEL2_MINICARD ||
-	 	RT_GetInterfaceSelection(Adapter) == INTF_SEL3_USB_Solo ||
-		RT_GetInterfaceSelection(Adapter) == INTF_SEL4_USB_Combo)
-	{
+	if (RT_GetInterfaceSelection(Adapter) == INTF_SEL2_MINICARD
+	 || RT_GetInterfaceSelection(Adapter) == INTF_SEL3_USB_Solo
+	 || RT_GetInterfaceSelection(Adapter) == INTF_SEL4_USB_Combo) {
 		LedCfg = rtw_read8(Adapter, REG_LEDCFG2);
 		RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("In SwLedON,LedAddr:%X LEDPIN=%d\n",REG_LEDCFG2, pLed->LedPin));
-		switch(pLed->LedPin)
-		{
-			case LED_PIN_GPIO0:
-				break;
+		switch(pLed->LedPin) {
+		case LED_PIN_GPIO0:
+			break;
 
-			case LED_PIN_LED0:
+		case LED_PIN_LED0:
 
-				RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("In SwLedOn,LedAddr:%X LEDPIN=%d\n",REG_LEDCFG2, pLed->LedPin));
-				LedCfg = rtw_read8(Adapter, REG_LEDCFG2);
-				rtw_write8(Adapter, REG_LEDCFG2, (LedCfg&0xf0)|BIT5|BIT6); // SW control led0 on.
-				break;
+			RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("In SwLedOn,LedAddr:%X LEDPIN=%d\n",REG_LEDCFG2, pLed->LedPin));
+			LedCfg = rtw_read8(Adapter, REG_LEDCFG2);
+			rtw_write8(Adapter, REG_LEDCFG2, (LedCfg&0xf0)|BIT5|BIT6); /* SW control led0 on. */
+			break;
 
-			case LED_PIN_LED1:
-				rtw_write8(Adapter, REG_LEDCFG2, (LedCfg&0x0f)|BIT5); // SW control led1 on.
-				break;
+		case LED_PIN_LED1:
+			rtw_write8(Adapter, REG_LEDCFG2, (LedCfg&0x0f)|BIT5); /* SW control led1 on. */
+			break;
 
-			default:
-				break;
+		default:
+			break;
 
 		}
-	}
-	else
-	{
-		switch(pLed->LedPin)
-		{
-			case LED_PIN_GPIO0:
-				break;
+	} else {
+		switch(pLed->LedPin) {
+		case LED_PIN_GPIO0:
+			break;
 
-			case LED_PIN_LED0:
-			case LED_PIN_LED1:
-			case LED_PIN_LED2:
-				if(IS_HARDWARE_TYPE_8821U(Adapter))
-				{
-					LedCfg = rtw_read8(Adapter, REG_LEDCFG2);
-					rtw_write8(Adapter, REG_LEDCFG2, ((LedCfg&0x20) & (~BIT3))|BIT5); // SW control led0 on.
-					RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("8821 SwLedOn LED2 0x%x\n", rtw_read32(Adapter, REG_LEDCFG0)));
-				}
+		case LED_PIN_LED0:
+		case LED_PIN_LED1:
+		case LED_PIN_LED2:
+			if(IS_HARDWARE_TYPE_8821U(Adapter)) {
+				LedCfg = rtw_read8(Adapter, REG_LEDCFG2);
+				rtw_write8(Adapter, REG_LEDCFG2, ((LedCfg&0x20) & (~BIT3))|BIT5); // SW control led0 on.
+				RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("8821 SwLedOn LED2 0x%x\n", rtw_read32(Adapter, REG_LEDCFG0)));
+			}
 
-				break;
+			break;
 
-			default:
-				break;
+		default:
+			break;
 		}
 	}
 	pLed->bLedOn = _TRUE;
 }
 
 
-//
-//	Description:
-//		Turn off LED according to LedPin specified.
-//
-static void
-SwLedOff_8821AU(
-	PADAPTER		Adapter,
-	PLED_USB		pLed
-)
+/*
+ * Description:
+ * Turn off LED according to LedPin specified.
+ */
+
+static void SwLedOff_8821AU(PADAPTER Adapter,PLED_USB pLed)
 {
 	HAL_DATA_TYPE *pHalData = GET_HAL_DATA(Adapter);
 	uint8_t	LedCfg;
 
-	if(Adapter->bSurpriseRemoved == _TRUE)
-	{
+	if (Adapter->bSurpriseRemoved == _TRUE) {
 		return;
 	}
 
-	if(	RT_GetInterfaceSelection(Adapter) == INTF_SEL2_MINICARD ||
-		RT_GetInterfaceSelection(Adapter) == INTF_SEL3_USB_Solo ||
-		RT_GetInterfaceSelection(Adapter) == INTF_SEL4_USB_Combo)
-	{
+	if (RT_GetInterfaceSelection(Adapter) == INTF_SEL2_MINICARD
+	 || RT_GetInterfaceSelection(Adapter) == INTF_SEL3_USB_Solo
+	 || RT_GetInterfaceSelection(Adapter) == INTF_SEL4_USB_Combo) {
 		RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("In SwLedOff,LedAddr:%X LEDPIN=%d\n",REG_LEDCFG2, pLed->LedPin));
 		LedCfg = rtw_read8(Adapter, REG_LEDCFG2);
 
-		// 2009/10/23 MH Issau eed to move the LED GPIO from bit  0 to bit3.
-		// 2009/10/26 MH Issau if tyhe device is 8c DID is 0x8176, we need to enable bit6 to
-		// enable GPIO8 for controlling LED.
-		// 2010/07/02 Supprt Open-drain arrangement for controlling the LED. Added by Roger.
-		//
-		switch(pLed->LedPin)
-		{
+		/*
+		 * 2009/10/23 MH Issau eed to move the LED GPIO from bit  0 to bit3.
+		 * 2009/10/26 MH Issau if tyhe device is 8c DID is 0x8176, we need to enable bit6 to
+		 * enable GPIO8 for controlling LED.
+		 * 2010/07/02 Supprt Open-drain arrangement for controlling the LED. Added by Roger.
+		 */
 
+		switch(pLed->LedPin) {
 			case LED_PIN_GPIO0:
 				break;
 
 			case LED_PIN_LED0:
-				if(pHalData->bLedOpenDrain == _TRUE)
-				{
-					LedCfg &= 0x90; // Set to software control.
+				if(pHalData->bLedOpenDrain == _TRUE) {
+					LedCfg &= 0x90; /* Set to software control. */
 					rtw_write8(Adapter, REG_LEDCFG2, (LedCfg|BIT3));
 					LedCfg = rtw_read8(Adapter, REG_MAC_PINMUX_CFG);
 					LedCfg &= 0xFE;
@@ -351,28 +287,24 @@ SwLedOff_8821AU(
 				break;
 
 			case LED_PIN_LED1:
-				LedCfg &= 0x0f; // Set to software control.
+				LedCfg &= 0x0f; /* Set to software control. */
 				rtw_write8(Adapter, REG_LEDCFG2, (LedCfg|BIT3));
 				break;
 
 			default:
 				break;
 		}
-	}
-	else
-	{
-		switch(pLed->LedPin)
-		{
+	} else {
+		switch(pLed->LedPin) {
 			case LED_PIN_GPIO0:
 				break;
 
 			case LED_PIN_LED0:
 			case LED_PIN_LED1:
 			case LED_PIN_LED2:
-				 if(IS_HARDWARE_TYPE_8821U(Adapter))
-				 {
+				 if(IS_HARDWARE_TYPE_8821U(Adapter)) {
 					LedCfg = rtw_read8(Adapter, REG_LEDCFG2);
-					LedCfg &= 0x20; // Set to software control.
+					LedCfg &= 0x20; 	/* Set to software control. */
 					rtw_write8(Adapter, REG_LEDCFG2, (LedCfg|BIT3|BIT5));
 					RT_TRACE(_module_rtl8712_led_c_,_drv_info_,("8821  SwLedOff LED2 0x%x\n", rtw_read32(Adapter, REG_LEDCFG0)));
 				 }
@@ -388,24 +320,11 @@ SwLedOff_8821AU(
 	pLed->bLedOn = _FALSE;
 }
 
-
-//================================================================================
-// Interface to manipulate LED objects.
-//================================================================================
-
-
-//================================================================================
-// Default LED behavior.
-//================================================================================
-
-//
-//	Description:
-//		Initialize all LED_871x objects.
-//
-void
-rtl8812au_InitSwLeds(
-	_adapter	*padapter
-	)
+/*
+ * Description:
+ * Initialize all LED_871x objects.
+ */
+void rtl8812au_InitSwLeds(_adapter *padapter)
 {
 	struct led_priv *pledpriv = &(padapter->ledpriv);
 
@@ -420,26 +339,20 @@ rtl8812au_InitSwLeds(
 	}
 
 	InitLed(padapter, &(pledpriv->SwLed0), LED_PIN_LED0);
-
 	InitLed(padapter, &(pledpriv->SwLed1), LED_PIN_LED1);
-
 	InitLed(padapter, &(pledpriv->SwLed2), LED_PIN_LED2);
 }
 
-
-//
-//	Description:
-//		DeInitialize all LED_819xUsb objects.
-//
-void
-rtl8812au_DeInitSwLeds(
-	_adapter	*padapter
-	)
+/*
+ * Description:
+ * DeInitialize all LED_819xUsb objects.
+ */
+void rtl8812au_DeInitSwLeds(_adapter *padapter)
 {
 	struct led_priv	*ledpriv = &(padapter->ledpriv);
 
-	DeInitLed( &(ledpriv->SwLed0) );
-	DeInitLed( &(ledpriv->SwLed1) );
-	DeInitLed( &(ledpriv->SwLed2) );
+	DeInitLed(&(ledpriv->SwLed0) );
+	DeInitLed(&(ledpriv->SwLed1) );
+	DeInitLed(&(ledpriv->SwLed2) );
 }
 

6d0c5c9632bb9bec8b9c996c30ddd162a3a80430
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2014-09-21 20:54:24 +0200

    remove CONFIG_EFUSE_CONFIG_FILE code
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- include/osdep_service_linux.h
+++ include/osdep_service_linux.h
@@ -78,10 +78,6 @@
 	#include <linux/earlysuspend.h>
 #endif //CONFIG_HAS_EARLYSUSPEND
 
-#ifdef CONFIG_EFUSE_CONFIG_FILE
-	#include <linux/fs.h>
-#endif //CONFIG_EFUSE_CONFIG_FILE
-
 #ifdef CONFIG_USB_HCI
 	#include <linux/usb.h>
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,21))

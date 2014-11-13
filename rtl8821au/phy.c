44fde4e83e92dabb8b82b423d0a73f73888f1ea6
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2014-11-13 12:27:59 +0100

    change param in call of _rtl8812au_iqk_backup_rf() to rtl_priv
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- rtl8821au/phy.c
+++ rtl8821au/phy.c
@@ -1270,11 +1270,14 @@ static void _rtl8821au_iqk_backup_macbb(struct rtl_priv *rtlpriv,
 	/* ODM_RT_TRACE(pDM_Odm, ODM_COMP_CALIBRATION, ODM_DBG_LOUD, ("BackupMacBB Success!!!!\n")); */
 }
 
-static void _rtl8812au_iqk_backup_rf(struct rtl_dm *pDM_Odm,
+static void _rtl8812au_iqk_backup_rf(struct rtl_priv *rtlpriv,
 	uint32_t *RFA_backup, uint32_t *RFB_backup,
 	uint32_t *Backup_RF_REG, uint32_t RF_NUM)
 {
 	uint32_t i;
+	struct rtw_hal	*pHalData = GET_HAL_DATA(rtlpriv);
+	struct rtl_dm *	pDM_Odm = &pHalData->odmpriv;
+	
 	ODM_SetBBReg(pDM_Odm, 0x82c, BIT(31), 0x0); /* [31] = 0 --> Page C */
 	/* Save RF Parameters */
 	for (i = 0; i < RF_NUM; i++) {
@@ -1775,7 +1778,7 @@ static void _rtl8812au_phy_iq_calibrate(struct rtl_priv *rtlpriv)
 
 	_rtl8812au_iqk_backup_macbb(rtlpriv, MACBB_backup, Backup_MACBB_REG, MACBB_REG_NUM);
 	_rtl8812au_iqk_backup_afe(rtlpriv, AFE_backup, Backup_AFE_REG, AFE_REG_NUM);
-	_rtl8812au_iqk_backup_rf(pDM_Odm, RFA_backup, RFB_backup, Backup_RF_REG, RF_REG_NUM);
+	_rtl8812au_iqk_backup_rf(rtlpriv, RFA_backup, RFB_backup, Backup_RF_REG, RF_REG_NUM);
 
 	_rtl8812au_iqk_configure_mac(pDM_Odm);
 	_rtl8812au_iqk_tx(pDM_Odm, ODM_RF_PATH_A);

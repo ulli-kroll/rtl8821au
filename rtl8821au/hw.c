477cbd04b8781953faaef522e2309106211c5394
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2014-12-03 12:43:26 +0100

    simplify rtl8821au_set_hw_reg()
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- rtl8821au/hw.c
+++ rtl8821au/hw.c
@@ -296,7 +296,7 @@ static void Hal_PatchwithJaguar_8812(struct rtl_priv *Adapter, RT_MEDIA_STATUS	M
 	}
 }
 
-static void SetHwReg8812A(struct rtl_priv *padapter, uint8_t variable, uint8_t *pval)
+void rtl8821au_set_hw_reg(struct rtl_priv *padapter, u8 variable, u8 *pval)
 {
 	struct rtw_hal *pHalData;
 	struct dm_priv *pdmpriv;
@@ -310,6 +310,43 @@ static void SetHwReg8812A(struct rtl_priv *padapter, uint8_t variable, uint8_t *
 	podmpriv = &pHalData->odmpriv;
 
 	switch (variable) {
+	case HW_VAR_RXDMA_AGG_PG_TH:
+#ifdef CONFIG_USB_RX_AGGREGATION
+		{
+			/*uint8_t	threshold = *((uint8_t *)val);
+			if ( threshold == 0)
+			{
+				threshold = pHalData->UsbRxAggPageCount;
+			}
+			rtw_write8(Adapter, REG_RXDMA_AGG_PG_TH, threshold);*/
+		}
+#endif
+		break;
+	case HW_VAR_SET_RPWM:
+		break;
+	case HW_VAR_USB_MODE:
+		if (*pval == 1)
+			rtw_write8(padapter, REG_OPT_CTRL_8812, 0x4);
+		else if (*pval == 2)
+			rtw_write8(padapter, REG_OPT_CTRL_8812, 0x8);
+
+		rtw_write8(padapter, REG_SDIO_CTRL_8812, 0x2);
+		rtw_write8(padapter, REG_ACLK_MON, 0x1);
+		/*
+		 * 2013/01/29 MH Test with chunchu/cheng, in Alpha AMD platform. when
+		 * U2/U3 switch 8812AU will be recognized as another card and then
+		 * OS will treat it as a new card and assign a new GUID. Then SWUSB
+		 * service can not work well. We need to delay the card switch time to U3
+		 * Then OS can unload the previous U2 port card and load new U3 port card later.
+		 * The strange sympton can disappear.
+		 */
+		rtw_write8(padapter, REG_CAL_TIMER+1, 0x40);
+		/* rtw_write8(padapter, REG_CAL_TIMER+1, 0x3); */
+		rtw_write8(padapter, REG_APS_FSMCO+1, 0x80);
+		break;
+		
+		
+		
 	case HW_VAR_MEDIA_STATUS:
 		val8 = rtw_read8(padapter, MSR) & 0x0c;
 		val8 |= *pval;
@@ -919,52 +956,6 @@ static void SetHwReg8812A(struct rtl_priv *padapter, uint8_t variable, uint8_t *
 	}
 }
 
-void rtl8821au_set_hw_reg(struct rtl_priv *Adapter, u8 variable,u8 *val)
-{
-	 struct rtw_hal	*pHalData = GET_HAL_DATA(Adapter);
-	struct dm_priv	*pdmpriv = &pHalData->dmpriv;
-	struct rtl_dm *podmpriv = &pHalData->odmpriv;
-
-	switch (variable) {
-	case HW_VAR_RXDMA_AGG_PG_TH:
-#ifdef CONFIG_USB_RX_AGGREGATION
-		{
-			/*uint8_t	threshold = *((uint8_t *)val);
-			if ( threshold == 0)
-			{
-				threshold = pHalData->UsbRxAggPageCount;
-			}
-			rtw_write8(Adapter, REG_RXDMA_AGG_PG_TH, threshold);*/
-		}
-#endif
-		break;
-	case HW_VAR_SET_RPWM:
-		break;
-	case HW_VAR_USB_MODE:
-		if (*val == 1)
-			rtw_write8(Adapter, REG_OPT_CTRL_8812, 0x4);
-		else if (*val == 2)
-			rtw_write8(Adapter, REG_OPT_CTRL_8812, 0x8);
-
-		rtw_write8(Adapter, REG_SDIO_CTRL_8812, 0x2);
-		rtw_write8(Adapter, REG_ACLK_MON, 0x1);
-		/*
-		 * 2013/01/29 MH Test with chunchu/cheng, in Alpha AMD platform. when
-		 * U2/U3 switch 8812AU will be recognized as another card and then
-		 * OS will treat it as a new card and assign a new GUID. Then SWUSB
-		 * service can not work well. We need to delay the card switch time to U3
-		 * Then OS can unload the previous U2 port card and load new U3 port card later.
-		 * The strange sympton can disappear.
-		 */
-		rtw_write8(Adapter, REG_CAL_TIMER+1, 0x40);
-		/* rtw_write8(Adapter, REG_CAL_TIMER+1, 0x3); */
-		rtw_write8(Adapter, REG_APS_FSMCO+1, 0x80);
-		break;
-	default:
-		SetHwReg8812A(Adapter, variable, val);
-		break;
-	}
-}
 
 void rtl8821au_get_hw_reg(struct rtl_priv *padapter, u8 variable,u8 *pval)
 {

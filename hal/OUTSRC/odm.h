b3fd0210c9e5547096ca05e06dc867275cbf8d27
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2015-01-11 17:59:07 +0100

    remove unused OFDMSwingTable
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- hal/OUTSRC/odm.h
+++ hal/OUTSRC/odm.h
@@ -1589,7 +1589,6 @@ typedef enum tag_SW_Antenna_Switch_Definition
 //
 // Extern Global Variables.
 //
-extern	uint32_t OFDMSwingTable[OFDM_TABLE_SIZE_92D];
 extern  uint32_t TxScalingTable_Jaguar[TXSCALE_TABLE_SIZE];
 
 // <20121018, Kordan> In case fail to read TxPowerTrack.txt, we use the table of 88E as the default table.

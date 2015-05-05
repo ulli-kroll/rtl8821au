a647da0a45983451960feb01e8b2a8639cc9c116
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2015-05-05 12:19:51 +0200

    move ODM_ReadAndConfig_MP_88??A_AGC_TAB to rtl8821au/phy.c
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- hal/HalHWImg8821A_BB.c
+++ hal/HalHWImg8821A_BB.c
@@ -58,69 +58,6 @@ static BOOLEAN CheckCondition(const uint32_t  Condition, const uint32_t  Hex)
 ******************************************************************************/
 
 
-void ODM_ReadAndConfig_MP_8821A_AGC_TAB(struct rtl_priv *rtlpriv)
-{
-	struct rtl_hal	*rtlhal = rtl_hal(rtlpriv);
-	
-	#define READ_NEXT_PAIR(v1, v2, i) do { i += 2; v1 = Array[i]; v2 = Array[i+1]; } while(0)
-
-	uint32_t hex = 0;
-	uint32_t i = 0;
-	u16 count = 0;
-	uint32_t *ptr_array = NULL;
-
-	/* ULLI : fixed values ?? */
-	u8  platform = ODM_CE;
-	u8 _interface = RTW_USB;
-	u8 board = rtlhal->board_type;
-
-	uint32_t ArrayLen = RTL8821AU_AGC_TAB_ARRAY_LEN;
-	uint32_t *Array = RTL8821AU_AGC_TAB_ARRAY;
-
-
-	hex += board;
-	hex += _interface << 8;
-	hex += platform << 16;
-	hex += 0xFF000000;
-
-	ODM_RT_TRACE(pDM_Odm, ODM_COMP_INIT, ODM_DBG_TRACE, ("===> ODM_ReadAndConfig_MP_8821A_AGC_TAB, hex = 0x%X\n", hex));
-
-	for (i = 0; i < ArrayLen; i += 2) {
-		uint32_t v1 = Array[i];
-		uint32_t v2 = Array[i+1];
-
-		// This (offset, data) pair meets the condition.
-		if (v1 < 0xCDCDCDCD ) {
-			odm_ConfigBB_AGC_8821A(rtlpriv, v1, bMaskDWord, v2);
-			continue;
-		} else {
-			// This line is the start line of branch.
-			if (!CheckCondition(Array[i], hex)) {
-				// Discard the following (offset, data) pairs.
-				READ_NEXT_PAIR(v1, v2, i);
-				while (v2 != 0xDEAD && v2 != 0xCDEF &&
-					v2 != 0xCDCD && i < ArrayLen -2) {
-						READ_NEXT_PAIR(v1, v2, i);
-				}
-				i -= 2; // prevent from for-loop += 2
-			} else {
-				// Configure matched pairs and skip to end of if-else.
-				READ_NEXT_PAIR(v1, v2, i);
-				while (v2 != 0xDEAD && v2 != 0xCDEF &&
-					v2 != 0xCDCD && i < ArrayLen -2) {
-						odm_ConfigBB_AGC_8821A(rtlpriv, v1, bMaskDWord, v2);
-						READ_NEXT_PAIR(v1, v2, i);
-				}
-
-				while (v2 != 0xDEAD && i < ArrayLen -2) {
-					READ_NEXT_PAIR(v1, v2, i);
-				}
-			}
-		}
-	}
-
-}
-
 /******************************************************************************
 *                           PHY_REG.TXT
 ******************************************************************************/

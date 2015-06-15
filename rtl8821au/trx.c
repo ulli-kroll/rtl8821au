080902b78cd6f9c1359a7e0b8d8a840212e74165
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2015-06-15 09:10:12 +0200

    refractoring ODM_PhyStatusQuery()
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- rtl8821au/trx.c
+++ rtl8821au/trx.c
@@ -1071,22 +1071,11 @@ static u16 odm_Cfo(s8 Value)
  * Endianness before calling this API
  */
 
-static void odm_RxPhyStatusJaguarSeries_Parsing(struct _rtw_dm *pDM_Odm,
-	PODM_PHY_INFO_T pPhyInfo, u8 *pPhyStatus, PODM_PACKET_INFO_T pPktinfo);
 static void odm_Process_RSSIForDM(struct _rtw_dm *pDM_Odm, PODM_PHY_INFO_T pPhyInfo,
 	PODM_PACKET_INFO_T pPktinfo);
 
 static void ODM_PhyStatusQuery(struct _rtw_dm *	pDM_Odm, PODM_PHY_INFO_T pPhyInfo,
 	u8 *pPhyStatus, PODM_PACKET_INFO_T pPktinfo)
-{
-	odm_RxPhyStatusJaguarSeries_Parsing(pDM_Odm, pPhyInfo,
-					pPhyStatus, pPktinfo);
-
-	odm_Process_RSSIForDM(pDM_Odm, pPhyInfo, pPktinfo);
-}
-
-static void odm_RxPhyStatusJaguarSeries_Parsing(struct _rtw_dm *pDM_Odm,
-	PODM_PHY_INFO_T pPhyInfo, u8 *pPhyStatus, PODM_PACKET_INFO_T pPktinfo)
 {
 	struct rtl_priv *rtlpriv = pDM_Odm->rtlpriv;
 	struct rtl_dm *rtldm = rtl_dm(rtlpriv);
@@ -1437,7 +1426,7 @@ static void odm_RxPhyStatusJaguarSeries_Parsing(struct _rtw_dm *pDM_Odm,
 	 * DbgPrint("pPhyInfo->RxMIMOSignalStrength[0]=%d, pPhyInfo->RxMIMOSignalStrength[1]=%d, RxPWDBAll=%d\n",
 	 *	pPhyInfo->RxMIMOSignalStrength[0], pPhyInfo->RxMIMOSignalStrength[1], pPhyInfo->RxPWDBAll);
 	 */
-
+	odm_Process_RSSIForDM(pDM_Odm, pPhyInfo, pPktinfo);
 }
 
 

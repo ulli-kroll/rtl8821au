a89b2ca41cc9b33dbf679df06ed34a04c20afa98
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2015-06-22 06:40:34 +0200

    use mapping for EFUSE_ANA8M
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- rtl8821au/sw.c
+++ rtl8821au/sw.c
@@ -120,6 +120,7 @@ static struct rtl_hal_cfg rtl8821au_hal_cfg = {
 	.maps[EFUSE_PWC_EV12V] = PWC_EV12V,
 	.maps[EFUSE_FEN_ELDR] = FEN_ELDR,
 	.maps[EFUSE_LOADER_CLK_EN] = LOADER_CLK_EN,
+	.maps[EFUSE_ANA8M] = ANA8M,
 };
 
 

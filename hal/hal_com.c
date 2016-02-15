ff8732ca79a9a6223954912f9dd83364118f29c1
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2016-02-15 09:10:32 +0100

    remove unsed c2h_evt_clear(), c2h_evt_read()
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- hal/hal_com.c
+++ hal/hal_com.c
@@ -119,69 +119,6 @@ void hal_init_macaddr(struct rtl_priv *rtlpriv)
 	rtlpriv->cfg->ops->set_hw_reg(rtlpriv, HW_VAR_ETHER_ADDR, rtlpriv->mac80211.mac_addr);
 }
 
-/*
-* C2H event format:
-* Field	 TRIGGER		CONTENT	   CMD_SEQ 	CMD_LEN		 CMD_ID
-* BITS	 [127:120]	[119:16]      [15:8]		  [7:4]	 	   [3:0]
-*/
-
-void c2h_evt_clear(struct rtl_priv *rtlpriv)
-{
-	rtl_write_byte(rtlpriv, REG_C2HEVT_CLEAR, C2H_EVT_HOST_CLOSE);
-}
-
-int32_t c2h_evt_read(struct rtl_priv *rtlpriv, uint8_t *buf)
-{
-	int32_t ret = _FAIL;
-	struct c2h_evt_hdr *c2h_evt;
-	int i;
-	uint8_t trigger;
-
-	if (buf == NULL)
-		goto exit;
-
-	trigger = rtl_read_byte(rtlpriv, REG_C2HEVT_CLEAR);
-
-	if (trigger == C2H_EVT_HOST_CLOSE) {
-		goto exit; /* Not ready */
-	} else if (trigger != C2H_EVT_FW_CLOSE) {
-		goto clear_evt; /* Not a valid value */
-	}
-
-	c2h_evt = (struct c2h_evt_hdr *)buf;
-
-	memset(c2h_evt, 0, 16);
-
-	*buf = rtl_read_byte(rtlpriv, REG_C2HEVT_MSG_NORMAL);
-	*(buf+1) = rtl_read_byte(rtlpriv, REG_C2HEVT_MSG_NORMAL + 1);
-
-	RT_PRINT_DATA(rtlpriv, COMP_FW, DBG_DMESG, "c2h_evt_read(): ",
-		&c2h_evt , sizeof(c2h_evt));
-
-	if (0) {
-		RT_TRACE(rtlpriv, COMP_FW, DBG_LOUD, "%s id:%u, len:%u, seq:%u, trigger:0x%02x\n", __func__
-			, c2h_evt->id, c2h_evt->plen, c2h_evt->seq, trigger);
-	}
-
-	/* Read the content */
-	for (i = 0; i < c2h_evt->plen; i++)
-		c2h_evt->payload[i] = rtl_read_byte(rtlpriv, REG_C2HEVT_MSG_NORMAL + sizeof(*c2h_evt) + i);
-
-	RT_PRINT_DATA(rtlpriv, COMP_FW, DBG_DMESG, "c2h_evt_read(): Command Content:\n",
-		c2h_evt->payload, c2h_evt->plen);
-
-	ret = _SUCCESS;
-
-clear_evt:
-	/*
-	* Clear event to notify FW we have read the command.
-	* If this field isn't clear, the FW won't update the next command message.
-	*/
-	c2h_evt_clear(rtlpriv);
-exit:
-	return ret;
-}
-
 uint8_t  rtw_hal_networktype_to_raid(struct rtl_priv *rtlpriv,unsigned char network_type)
 {
 	return networktype_to_raid_ex(rtlpriv,network_type);

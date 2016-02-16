ed75275e43ba316bb987751dc4495c9b7d94c15d
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2016-02-16 12:20:03 +0100

    rename swing tables
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- rtl8821au/dm.h
+++ rtl8821au/dm.h
@@ -28,15 +28,6 @@
 
 #define CCK_TABLE_SIZE				33
 
-
-
-
-
-extern	u8 CCKSwingTable_Ch1_Ch13[CCK_TABLE_SIZE][8];
-extern	u8 CCKSwingTable_Ch14 [CCK_TABLE_SIZE][8];
-extern	u8 CCKSwingTable_Ch1_Ch13_New[CCK_TABLE_SIZE][8];
-extern	u8 CCKSwingTable_Ch14_New [CCK_TABLE_SIZE][8];
-
 void rtl8812au_get_delta_swing_table(struct rtl_priv *rtlpriv,
 					    u8 **up_a, u8 **down_a,
 					    u8 **up_b, u8 **down_b);

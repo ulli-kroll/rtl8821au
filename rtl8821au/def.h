4316e5383a92fc90267963dbbf49fdefb1225767
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2016-06-13 06:41:53 +0200

    move board_types to def.h
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- rtl8821au/def.h
+++ rtl8821au/def.h
@@ -93,6 +93,18 @@ enum MGN_RATE{
 	MGN_UNKNOWN
 };
 
+enum board_type {
+    ODM_BOARD_DEFAULT  	= 0, 	  // The DEFAULT case.
+    ODM_BOARD_MINICARD  = BIT(0), // 0 = non-mini card, 1= mini card.
+    ODM_BOARD_SLIM      = BIT(1), // 0 = non-slim card, 1 = slim card
+    ODM_BOARD_BT        = BIT(2), // 0 = without BT card, 1 = with BT
+    ODM_BOARD_EXT_PA    = BIT(3), // 0 = no 2G ext-PA, 1 = existing 2G ext-PA
+    ODM_BOARD_EXT_LNA   = BIT(4), // 0 = no 2G ext-LNA, 1 = existing 2G ext-LNA
+    ODM_BOARD_EXT_TRSW  = BIT(5), // 0 = no ext-TRSW, 1 = existing ext-TRSW
+    ODM_BOARD_EXT_PA_5G    = BIT(6), // 0 = no 5G ext-PA, 1 = existing 5G ext-PA
+    ODM_BOARD_EXT_LNA_5G   = BIT(7), // 0 = no 5G ext-LNA, 1 = existing 5G ext-LNA
+};
+
 enum rtl_desc_qsel {
 	QSLT_BK = 0x2,
 	QSLT_BE = 0x0,
@@ -124,7 +136,7 @@ enum version_8821au {
 	/* ULLI : our own bitfield, form pHalData->bSupportUSB3 */
 
 	RTL8821AU_USB3_MODE = BIT(15),
-	
+
 	VERSION_UNKNOWN = 0x0,
 };
 
--- wifi.h
+++ wifi.h
@@ -607,18 +607,6 @@ enum hw_variables {
 	HW_VAR_WIRELESS_MODE,
 };
 
-typedef enum tag_Board_Definition
-{
-    ODM_BOARD_DEFAULT  	= 0, 	  // The DEFAULT case.
-    ODM_BOARD_MINICARD  = BIT(0), // 0 = non-mini card, 1= mini card.
-    ODM_BOARD_SLIM      = BIT(1), // 0 = non-slim card, 1 = slim card
-    ODM_BOARD_BT        = BIT(2), // 0 = without BT card, 1 = with BT
-    ODM_BOARD_EXT_PA    = BIT(3), // 0 = no 2G ext-PA, 1 = existing 2G ext-PA
-    ODM_BOARD_EXT_LNA   = BIT(4), // 0 = no 2G ext-LNA, 1 = existing 2G ext-LNA
-    ODM_BOARD_EXT_TRSW  = BIT(5), // 0 = no ext-TRSW, 1 = existing ext-TRSW
-    ODM_BOARD_EXT_PA_5G    = BIT(6), // 0 = no 5G ext-PA, 1 = existing 5G ext-PA
-    ODM_BOARD_EXT_LNA_5G   = BIT(7), // 0 = no 5G ext-LNA, 1 = existing 5G ext-LNA
-}ODM_BOARD_TYPE_E;
 
 enum band_type {
 	BAND_ON_2_4G = 0,
